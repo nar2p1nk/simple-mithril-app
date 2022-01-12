b333a8c669c04301e8b9a9aa940600507356dc48
nar2p1nk <farrisisqandar@gmail.com>
2022-01-12 23:44:53 +0800

    fixed the webpack,created user and UserList js

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -5,7 +5,9 @@ module.exports = {
     oninit: User.loadList,
     view:()=>{
         return m('.user-list',User.list.map((user)=>{
-            return m('.user-list-item', `${user.firstName} ${user.lastName}`)
+            return m('.user-list-item',[
+                m('p',`${user.firstName} ${user.lastName}, user id:${user.id}`)
+            ])
         }))
     }
 }
